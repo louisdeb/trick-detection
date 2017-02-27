189c508bbd29ba64c932cc642c5bff39d98f5874
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2017-02-27 21:13:51 +0000

    Correctly compile client code

--- src/client/comms.c
+++ src/client/comms.c
@@ -11,7 +11,7 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
   mpu_values mpu_reading;
   memcpy(&mpu_reading, packetbuf_dataptr(), sizeof mpu_reading);
-
+  
   printf("Received mpu readings, a_x: %d\n", mpu_reading.a_x);
 }
 
