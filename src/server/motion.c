fb0434ae8ab1730500a9fac26b82209d2288dd00
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2017-02-28 13:50:38 +0000

    Store first 10 mpu_readings

--- src/server/motion.c
+++ src/server/motion.c
@@ -1,13 +1,35 @@
 #include "motion.h"
 #include <stdio.h>
 
+mpu_values* front_readings[10];
+
 void process_packet(comms_packet packet)
 {
-  printf("Node Id: %s\n", packet.node_id);
-  printf("g_x: %d\n", (int) packet.mpu_reading.g_x);
-  printf("g_y: %d\n", (int) packet.mpu_reading.g_y);
-  printf("g_z: %d\n", (int) packet.mpu_reading.g_z);
-  printf("a_x: %d\n", packet.mpu_reading.a_x);
-  printf("a_y: %d\n", packet.mpu_reading.a_y);
-  printf("a_z: %d\n", packet.mpu_reading.a_z);
+  printf("Node Id: %d\n", packet.node_id);
+  print_reading(packet.mpu_reading);
+  add_reading_front(packet.mpu_reading);
+}
+
+void add_reading_front(mpu_values reading)
+{
+  for (int i = 0; i < 10; i++) {
+    if (front_readings[i] == NULL) {
+      front_readings[i] = &reading;
+      return;
+    }
+  }
+  // stores only 10 readings
+  for (int i = 0; i < 10; i++) {
+    print_reading(*front_readings[i]);
+  }
+}
+
+void print_reading(mpu_values reading)
+{
+  printf("g_x: %d\n", (int) reading.g_x);
+  printf("g_y: %d\n", (int) reading.g_y);
+  printf("g_z: %d\n", (int) reading.g_z);
+  printf("a_x: %d\n", reading.a_x);
+  printf("a_y: %d\n", reading.a_y);
+  printf("a_z: %d\n", reading.a_z);
 }
--- src/server/motion.h
+++ src/server/motion.h
@@ -3,3 +3,5 @@
 /*---------------------------------------------------------------------------*/
 
 void process_packet(comms_packet);
+void add_reading_front(mpu_values);
+void print_reading(mpu_values);
