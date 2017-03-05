a8c2a5887c3032f861cd525859c54260e363b806
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-05 18:56:04 +0000

    Remove pop for testing flips

--- src/server/motion.c
+++ src/server/motion.c
@@ -12,7 +12,7 @@
 #define DIRECTION_LEEWAY    10
 
 #define ROTATION_THRESHOLD  150
-#define VERTICAL_THRESHOLD  10
+#define VERTICAL_THRESHOLD  5
 
 #define BOTTOM_THRESHOLD    (DIRECTION_TRUE - DIRECTION_LEEWAY)
 #define TOP_THRESHOLD       (DIRECTION_TRUE + DIRECTION_LEEWAY)
@@ -30,10 +30,10 @@ void process_packet(comms_packet packet)
   add_reading(packet.node_id, packet.mpu_reading);
 
   Roll roll = detect_roll(history);
-  Pop pop = detect_pop(front_history, back_history);
+  //Pop pop = detect_pop(front_history, back_history);
   Spin spin = detect_spin(front_history);
 
-  Trick trick = get_trick(roll, pop, spin);
+  Trick trick = get_trick(roll, tail, spin);
   if (trick != no_trick) {
     printf("Trick: %s\n", get_trick_string(trick));
   }
